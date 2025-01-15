d4417c5092886a3051aad7624d8866c78895f1ed
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-01-15 13:04:14 -0400

    f

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
+"""
+This script extracts specific parts of the string "School" and prints them:
+- The first 3 letters of the string.
+- The last 2 letters of the string.
+- The middle part of the string (excluding the first and last characters).
+"""
+
 word = "Holberton"
 
-
-print(f"First 3 letters: {word[0:3]}")
-print(f"Last 2 letters: {word[7:9]}")
-print(f"Middle word: {word[1:8]}")
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
