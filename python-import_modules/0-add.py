cc9e74b03201901d131d1d0f58e19bc0baeda020
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-01-20 09:06:34 -0400

    Fix syntax error in add_0.py by closing return statement parentheses

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+from add_0 import add
 
-if __name__ == "__main__":
-    # Import add function from add_0.py
-    from add_0 import add
+a = 1
+b = 2
 
-    # Define the values of a and b
-    a = 1
-    b = 2
-
-    # Call the add function and print the formatted result
-    print(f"{a} + {b} = {add(a, b)}")
+print("{} + {} = {}".format(a, b, add(a, b)))
