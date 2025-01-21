397767683c58c8443df938a273042048921ec4de
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-01-21 18:38:21 -0400

    fix

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,11 +1,2 @@
 def no_c(my_string):
-    # Initialize an empty string to store the result
-    result = ""
-
-    # Iterate through each character in the input string
-    for char in my_string:
-        # If the character is not 'c' or 'C', add it to the result
-        if char != 'c' and char != 'C':
-            result += char
-
-    return result
+    return ''.join(char for char in my_string if char not in 'cC')
