9193e5662619844ea325e7405030c2c22613d9c3
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-02-14 10:49:13 -0400

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-import sys
-import json
-from os import path
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 """
 This script adds all command-line arguments to a Python list
 and saves them to a file as a JSON representation.
 """
 
+import sys
+import json
+from os import path
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 
