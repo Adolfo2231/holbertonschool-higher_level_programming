2ce2f79778de24502763f24af5ee4a4edce15412
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-03-07 12:13:45 -0400

    create user with password

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+--create user user_0d_1 with password user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+--create user user_0d_2 with password user_0d_2_pwd
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
