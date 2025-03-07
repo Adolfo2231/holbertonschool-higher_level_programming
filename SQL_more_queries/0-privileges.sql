61ef37c5be271a3748f6ef924025a7ced457316c
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-03-07 13:10:36 -0400

    fix

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
---show grants for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
---show grants for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Show privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1' @'localhost';
+
+-- Show privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2' @'localhost';
