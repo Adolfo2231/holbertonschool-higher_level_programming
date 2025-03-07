5c7ed70367255467fbeb977419670a4435c83d3f
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-03-07 13:55:54 -0400

    fix

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
---create table unique_id
+-- Create the table unique_id if it does not exist
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
+    id INT NOT NULL DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
