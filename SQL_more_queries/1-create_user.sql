61d9352a83a37803d1b329e4e294602880396c77
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-03-07 13:09:30 -0400

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,10 @@
---create user user_0d_1 with password user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
---create user user_0d_2 with password user_0d_2_pwd
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Create user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost'
+WITH
+GRANT OPTION;
+
+-- Apply changes
+FLUSH PRIVILEGES;
